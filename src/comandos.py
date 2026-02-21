5a4395d4b89005578900cc70bd27da8ecafd7987
JAlexis1 <ruizquicenojhonalexis@gmail.com>
2026-02-21 18:16:38 -0500

    fix: standardize command functions to receive cmd argument

--- src/comandos.py
+++ src/comandos.py
@@ -2,15 +2,13 @@ from datetime import datetime
 import webbrowser
 import urllib.parse
 
-from voz_archivo import reconocer_voz
-
-def saludar():
+def saludar(texto):
     print("¡Hola! bienvenido al sistema de comandos por voz.")
 
-def mostrar_hora():
+def mostrar_hora(texto):
     print("Hora actual:", datetime.now().strftime("%H:%M"))
 
-def abrir_google():
+def abrir_google(texto):
     webbrowser.open("https://www.google.com")
 
 def buscar_youtube(texto):
@@ -20,16 +18,11 @@ def buscar_youtube(texto):
         url = f"https://www.youtube.com/results?search_query={urllib.parse.quote(consulta)}"
         webbrowser.open(url)
     else:
-        print("No entendí qué buscar en YouTube.")
-
-def salir():
-    print("Hasta luego, gracias por usar nuestro sistema de comandos por voz.")
-    exit()
+        print("No se entendió qué buscar en YouTube.")
 
 commands = {
     "hola": saludar,
     "hora": mostrar_hora,
     "abrir google": abrir_google,
     "busca en youtube": buscar_youtube,
-    "salir": salir
 }
