2fd13ff5609e06f54a1921fd66e3309a0ee8c150
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2026-02-25 23:00:07 -0500

    feat(comandos): nueva funcionalidad para abrir una aplicación del sistema

--- src/comandos.py
+++ src/comandos.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import webbrowser
 import urllib.parse
+import os
+import platform
 
 def saludar(texto):
     print("¡Hola! bienvenido al sistema de comandos por voz.")
@@ -20,9 +22,43 @@ def buscar_youtube(texto):
     else:
         print("No se entendió qué buscar en YouTube.")
 
+def abrir_aplicacion_sistema(texto: str):
+    sistema = platform.system()
+
+    apps = {
+        "calculadora": {
+            "Windows": "calc",
+            "Darwin": "open -a Calculator",
+            "Linux": "gnome-calculator || kcalc"
+        },
+        "bloc de notas": {
+            "Windows": "notepad",
+            "Darwin": "open -a TextEdit",
+            "Linux": "gedit || mousepad || xed || gnome-text-editor"
+        },
+        "terminal": {
+            "Windows": "start cmd",
+            "Darwin": "open -a Terminal",
+            "Linux": "gnome-terminal || xterm || konsole || terminal"
+        }
+    }
+
+    for app, os_systems in apps.items():
+        if app in texto:
+            command = os_systems.get(sistema)
+
+            if command:
+                print(f"Detectado {sistema}. Abriendo la aplicación del sistema...")
+                os.system(os_systems[sistema])
+
+                return
+
+    print("Aplicación no soportada en este sistema operativo.")
+
 commands = {
     "hola": saludar,
     "hora": mostrar_hora,
     "abrir google": abrir_google,
     "busca en youtube": buscar_youtube,
+    "abrir": abrir_aplicacion_sistema
 }
